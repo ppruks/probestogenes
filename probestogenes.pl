b03665ed73d9fb3e95aa752861c3bfa04688d4d7
ppruks <ppruks@alumni.stanford.edu>
2016-03-09 21:43:49 +0200

    finish expression stream

--- probestogenes.pl
+++ probestogenes.pl
@@ -6,7 +6,12 @@ use warnings;
 # Sort files by chromosome number, chromosome start and chrosome end 
 
 my @sort_annotation = ( "sort", "-k2", "-o", "../../data/expression/sorted_annotation.txt", $ARGV[0] );
-my @sort_rohgenes = ( "sort", "-k4","-k5","-k6", "../../data/expression/sorted_rohgenes", $ARGV[1] );
+my @sort_rohgenes = ( "sort", "-k4","-k5","-k6", "-o", "../../data/expression/sorted_rohgenes", $ARGV[1] );
+my @sort_expression = ( "sort", "-k1", "-o", "../../data/expression/sorted_expression", $ARGV[2] );
+
+system(@sort_annotation);
+system(@sort_rohgenes);
+system(@sort_expression);
 
 # Create empty hash for probe to haplotype matching
 
@@ -26,7 +31,7 @@ shift @rohgenes_header;
 while( my $rohgenes_line = <$rohgenes> ) {
 
 	# Split line and store variables
-	my ( $roh_haplotype, $roh_gene_name, $roh_chr, $roh_chr_start, $roh_chr_end, $roh_strand) =  split /[ \t]/, $rohgenes_line;
+	my @rohgenes_line =  split /[ \t]/, $rohgenes_line;
 
 	# Open annotation file stream
 	open my $annotation, "<", "../../data/expression/sorted_annotation.txt" or die $!;
@@ -35,27 +40,59 @@ while( my $rohgenes_line = <$rohgenes> ) {
 	my @annotation_header = <$annotation>;
 	shift @annotation_header;
 
-	while(my $annotation_line = <$annotation>) {
+	# Fill up probe to haplotype hash  
+
+	while( my $annotation_line = <$annotation> ) {
 		
-		# Split line and store variables
-		my ( $platform, $annotation_probenumber, $annotation_gene_name, $annotation_chr, $annotation_chr_start, $annotation_chr_end, $annotation_probe, $annotation_seq ) = split /[ \t]/, $annotation_line;
+		# Split line, store line in an array, get reference to array
+		my @annotation_line = split /[ \t]/, $annotation_line;
+		my $annotation_line_ref = \@annotation_line;
 		
-		# Keep looking if annotation chromosome number is less than roh chromosome number
-		if( $annotation_chr <= roh_chr ) {
+		# Keep looking if annotation chromosome number is less or equal to roh chromosome number
+		if( $annotation_line[3] <= $rohgenes_line[3] ) {
 
-			#------------------------------------------- 
-
-			# If roh file chromosome number, start and match those of annotation file's, then... NEED TO STORE roh genedata in an array!
-			if ( $roh_chr == $ annotation_chr && $roh_chr_start == $annotation_chr_start && $roh_chr_end == $roh_chr_end ) {
-
+			# If roh file's chromosome number, start and end match those of annotation file's, then add (probenumber : rohgenes_data_ref) key:value pair to hash
+			if ( $rohgenes_line[3] == $ annotation_line[3] && $rohgenes_line[4] == $annotation_line[4] && $rohgenes_line[5] == $rohgenes_line[5] ) {
+				%probes_to_rohgenes = {
+					annotation_data[1] => $rohgenes_line_ref # map probe number to reference to rohgenes_line
+				};
 			}
-
-
 		} else {
 			last;
 		}
 	}
 }
+
+# Open expression file stream 
+
+open my $expression, "<", "../../data/expression/sorted_expression.txt" or die $!;
+
+# Skip header line
+
+my @expression_header = <$expression>;
+shift @annotation_header;
+
+# Loop over lines of expression file and use probe number to index into probes_to_rohgenes hash and print out gene data to STDOUT
+
+while( my $expression_line = <$expression> ) {
+
+	# Split line and store probe number
+	my @expression_line = split /[ \t]/, $expression_line;
+
+	# Check if the hash contains the key probenumber, if yes, then index into hash, dereference and print elements of array 
+	if( exists $probes_to_rohgenes{$expression_line[0]} ) {
+			for my $gene_data_value (@{$probes_to_rohgenes{$expression_line[0]}}) {
+				print $gene_data_value, " ";
+			}
+			for my $expression_value (@expression_line) {
+				print $expression_value, " "; 
+			}
+			print "\n";
+		} 
+}
+
+
+
 
 # 	1) open annotation file stream 
 #	2) skip header line
